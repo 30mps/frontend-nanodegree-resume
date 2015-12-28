20f9ee0c6ccd0c0311345b54afff8da947f155c3
30mps <spmenon30@gmail.com>
2015-12-28 02:29:24 -0800

    Modify helper.js contact info variables by adding a space to accomadate fornt-awesome icons. Formatted contact info varibale names and comment the generic contact info for the bio object

--- js/helper.js
+++ js/helper.js
@@ -16,12 +16,12 @@ var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr/>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
-var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLmobile = '<li class="flex-item"><span class="orange-text ">mobile</span><span class="white-text fa fa-phone">&nbsp;%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text fa fa-envelope">&nbsp;%data%</span></li>';
+var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text fa fa-twitter">&nbsp;%data%</span></li>';
+var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text fa fa-github">&nbsp;%data%</span></li>';
+var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">&nbsp;%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text fa fa-location-arrow">&nbsp;%data%</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic img-thumbnail" alt="Tumbnail photo of Smitha">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
